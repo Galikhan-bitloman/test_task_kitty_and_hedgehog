b2e6c04e3027ea7aa48638f913cd0db2cecb6861
galikhan <galihan.abdikarim@gmail.com>
2022-04-12 11:39:55 +0600

    password was fixed

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!7lv)hqd^$66)+c2ylvc2b+n5$+6ykr4v8vzc=io7njd=snbpq'
+SECRET_KEY = ...
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Apple123',
+        'PASSWORD': '...',
         'HOST': 'localhost',
         'PORT': '5432',
     }
